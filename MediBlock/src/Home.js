310bee5afc58dbd34a1cf589a4157cfc50ef613d
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-01-12 12:11:04 +0530

    Update Home

--- MediBlock/src/Home.js
+++ MediBlock/src/Home.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import { View, StyleSheet, TouchableOpacity, } from 'react-native';
-import { NavigationBtn } from './components/index.js';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { NavigationBtn, Card } from './components/index.js';
+import defaultStyles from './components/defaultStyles';
 
-const App = () => (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-        <NavigationBtn componentName="DoctorHome" title="Doctor" />
-        <NavigationBtn componentName="PatientHome" title="Patient" />
-    </View>
-);
+class Home extends React.Component {
+    static get options() {
+        return {
+            topBar: {
+                visible: false, height: 0
+            }
+        };
+    }
 
-export default App;
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center' }}>
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <Text style={{
+                        color: defaultStyles.FONT_COLOR_PRIMARY,
+                        fontSize: defaultStyles.FONT_SIZE_TITLE
+                    }}>
+                        Welcome to MediBlock
+            </Text>
+                </View>
+                <View style={{ flex: 1 }}>
+                    <NavigationBtn componentName="DoctorHome" title="Doctor" />
+                    <NavigationBtn componentName="PatientHome" title="Patient" />
+                </View>
+            </View>
+        )
+    }
+}
+
+export default Home;
