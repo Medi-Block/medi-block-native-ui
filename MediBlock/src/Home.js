e18b71664e09d34d9e80f04462ba98c506e2ad2e
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-01-12 12:03:04 +0530

    Add doctor and patient navigation btns

--- MediBlock/src/Home.js
+++ MediBlock/src/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { View, StyleSheet, TouchableOpacity, } from 'react-native';
+import { NavigationBtn } from './components/index.js';
 
 const App = () => (
     <View style={{ flex: 1, justifyContent: 'center' }}>
+        <NavigationBtn componentName="DoctorHome" title="Doctor" />
+        <NavigationBtn componentName="PatientHome" title="Patient" />
     </View>
 );
 
